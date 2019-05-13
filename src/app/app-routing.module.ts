631b87cdf78d11a4ba349288bd3819f461ce5030
Vetta <german@vetta.io>
2019-05-13 11:23:57 -0500

    edit function added.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SearchComponent } from './search/search.component';
+import { EditComponent } from './edit/edit.component';
 
 const routes: Routes = [
-  { path: '', component: SearchComponent },
+  { path: 'search', component: SearchComponent },
+  { path: '', redirectTo: '/search', pathMatch: 'full' },
+  { path: 'edit/:id', component: EditComponent },
 ];
 
 @NgModule({
